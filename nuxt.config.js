f6e1f6ce3aba3eaa717b4a908488e30195d6f87a
nakao107107 <fumiaki4handai@gmail.com>
2019-08-22 18:28:54 +0900

    custom cssファイル導入 fontawsome　導入

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,8 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.6.1/css/all.css' }
     ]
   },
   /*
@@ -25,6 +26,7 @@ export default {
   ** Global CSS
   */
   css: [
+    '~/assets/css/now-ui-kit.css'
   ],
   /*
   ** Plugins to load before mounting the App
